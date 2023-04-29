cd2b5b5a3293361f6b2c94bea94874e104934355
dheerajkrishna458 <dheerajkittu27@gmail.com>
2023-04-29 18:33:58 -0500

    files added to project

---
+++ src/main/java/com/dheerajvinyasalu/DMS/Repository/DogRepository.java
@@ -0,0 +1,9 @@
+package com.dheerajvinyasalu.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dheerajvinyasalu.DMS.Model.Dog;
+
+public interface DogRepository extends CrudRepository<Dog,Integer>{
+
+}
---
+++ src/main/java/com/dheerajvinyasalu/DMS/Repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.dheerajvinyasalu.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dheerajvinyasalu.DMS.Model.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
